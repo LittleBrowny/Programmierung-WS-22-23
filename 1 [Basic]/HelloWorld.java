69ec8df43014c2f2e78be3cd2b83459ae7b74fe8
LittleBrowny <114399961+LittleBrowny@users.noreply.github.com>
2022-10-12 09:16:58 +0200

    Initialisierung

---
+++ 1 [Basic]/HelloWorld.java
@@ -0,0 +1,10 @@
+//[x] 1.1 HelloWorld
+
+// Das vorliegende Programm gibt den String "..." auf der Kommandozeile aus. Veraendern Sie das Programm so, dass es "Hello, World!" ausgibt.
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
+
